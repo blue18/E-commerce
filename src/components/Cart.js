c0fad705e01e0d438e38b6b8539075627b73d70b
blue18 <garcb969@icloud.com>
2019-04-04 20:03:34 -0700

    adding bootstrap classes for UI design in cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,26 +2,67 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { showProducts } from '../actions/shoppingCartActions';
 
-class Cart extends React.Component {
-
-  componentDidMount() {
-    console.log(this.props.products);
-  }
-
-  render() {
+function Cart(props) {
 
     // Get list of products
-    const products = this.props.products;
+    const products = props.products;
+    let totalPrice = 0;
+    
+    products.map(product => {
+      totalPrice = parseFloat(product.price) + totalPrice;
+    });
 
     // Get name and price of products from list 
     const aProduct = products.map(function(product) {
-      return <li key={product.id} > { product.name } { product.price } </li>;
+      return (
+        <div key={parseInt(product.id)} className={`row`}>
+          <div className={`col`}> 
+            <img src={"https://www.computerhope.com/jargon/b/binary-0s-1s.jpg"} width="200" height="100" /> 
+          </div>
+          <div className={`col`}> {product.name} </div>
+          <div className={`col`}> Description... </div>
+          <div className={`col`}> {product.price} </div>
+          <div className={`col`}> 
+            <select>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+            </select>
+           </div>
+           <div className={`col`}> <button>Remove</button> </div>
+        </div>
+
+      );
     });
 
     return(
-      <div style={cartStyle}> { aProduct }</div>
+      <div className={`container`} style={allProducts}> 
+        {aProduct}
+        <div className={`row`}>
+          <div className={`col`}></div>
+          <div className={`col`}></div>
+          <div className={`col`} style={priceCol}>
+            <b>{`Total Price: $${totalPrice.toFixed(2)}`}</b>
+          </div>
+        </div>
+      </div>
     );
-  }
+}
+
+const priceCol = {
+  textAlign: "center",
+  fontSize: "40px"
+}
+
+const allProducts = {
+  border: "solid"
+}
+
+const productStyle = {
+  padding: "25px",
+  backgroundColor: "red",
+  border: "solid"
 }
 
 const cartStyle = {
